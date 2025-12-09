e8206006d6dfbc6af52b8cd0883d124ef9e023bd
Pooja <poojavartia52@gmail.com>
2025-12-09 08:27:48 +0530

    I have create this object which have been storing to the each kind of the time range into 4 divided parts

---
+++ BusTimming.js
@@ -0,0 +1,6 @@
+const timeSlots = {
+      morning: { start: "05:00", end: "11:59" },
+      afternoon: { start: "12:00", end: "16:59" },
+      evening: { start: "17:00", end: "20:59" },
+      night: { start: "21:00", end: "04:59" }
+    };
